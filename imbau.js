c72f0e1b41b2c0504b671cb8e5d467d2587cece5
septila22 <43529687+septila22@users.noreply.github.com>
2018-09-29 16:04:22 +0700

    Update imbau.js

--- imbau.js
+++ imbau.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="#"}
 if(x=="2"){xxx="#"}
 if(x=="3"){xxx="#"}
 if(x=="4"){xxx="#"}
-if(x=="5"){xxx="#"}a_to_ve[i].href="https://drama.aifawarung.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="#"}a_to_ve[i].href="https://iwak.aifawarung.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
