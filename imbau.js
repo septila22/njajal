b9eab70e73dfaec97359981074c7ab768f854572
septila22 <43529687+septila22@users.noreply.github.com>
2018-09-29 15:36:51 +0700

    Update imbau.js

--- imbau.js
+++ imbau.js
@@ -21,11 +21,11 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="/2018/09/google-opens-its-own-repair-center-in.html"}
+if(x=="1"){xxx="/2018/09/new-iphone-xs-max-ad-doubles-down-on.html"}
 if(x=="2"){xxx="/2018/09/google-opens-its-own-repair-center-in.html"}
 if(x=="3"){xxx="/2018/09/apple-releases-watchos-501-with-bug.html"}
 if(x=="4"){xxx="/2018/09/facebook-security-breach-affects-50.html"}
-if(x=="5"){xxx="/2018/09/the-redesigned-wear-os-21-by-google-is.html"}a_to_ve[i].href="http://drama.aifawarung.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2018/09/the-redesigned-wear-os-21-by-google-is.html"}a_to_ve[i].href="https://drama.aifawarung.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
